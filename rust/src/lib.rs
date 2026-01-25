5aeb494fd5a80fdfd790437d767850e623fdc1c6
Chesed <potatolegend200@gmail.com>
2026-01-26 00:32:27 +0530

    repurpose

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,53 +1,23 @@
-use godot::prelude::*;
+use godot::{classes::{IScriptLanguageExtension, ScriptLanguageExtension}, prelude::*};
 
 struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
 
-// use godot::classes::ISprite2D;
-// use godot::classes::Sprite2D;
+#[derive(GodotClass)]
+#[class(tool, init, base=ScriptLanguageExtension)]
+struct Corona {
+    base: Base<ScriptLanguageExtension>,
+}
 
-// #[derive(GodotClass)]
-// #[class(base=Sprite2D)]
-// struct Player {
-//     speed: f64,
-//     angular_speed: f64,
+#[godot_api]
+impl IScriptLanguageExtension for Corona {
+    fn get_name(&self) -> GString { "Corona".into() }
 
-//     base: Base<Sprite2D>,
-// }
+    fn get_type(&self) -> GString { "Corona".into() }
 
-// #[godot_api]
-// impl Player {
-//     #[func]
-//     fn increase_speed(&mut self, amount: f64) {
-//         self.speed += amount;
-//         self.base_mut().emit_signal("speed_increased", &[]);
-//     }
+    fn init_ext(&mut self) {}
 
-//     #[signal]
-//     pub fn speed_increased();
-// }
-
-// #[godot_api]
-// impl ISprite2D for Player {
-//     fn init(base: Base<Sprite2D>) -> Self {
-//         godot_print!("Hello, world!"); // Prints to the Godot console
-
-//         Self {
-//             speed: 400.0,
-//             angular_speed: std::f64::consts::PI,
-//             base,
-//         }
-//     }
-
-//     fn physics_process(&mut self, delta: f64) {
-//         // In GDScript, this would be:
-//         // rotation += angular_speed * delta
-//         let radians = (self.angular_speed * delta) as f32;
-//         self.base_mut().rotate(radians);
-//         // The 'rotate' method requires a f32,
-//         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
-//     }
-// }
-
+    
+}
